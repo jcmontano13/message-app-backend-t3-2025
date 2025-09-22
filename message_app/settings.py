6dadd182852a53e27c7481af6f11af20896e671c
jcmontano13 <jeck.montano@yahoo.com>
2025-09-22 16:13:36 +1200

    1. fix bug in settings.py

--- message_app/settings.py
+++ message_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0s7ie1zu6rs_l&qx3e0qk*o8fk5qaw3r_tq4=u9dkyt&kwtog8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
